8174b3792975edde04d831847473a45fa39ec106
31804 <3180436243@qq.com>
2021-07-15 12:27:43 +0800

    001

--- Re_0_003/Re_0_YX_01.py
+++ Re_0_003/Re_0_YX_01.py
@@ -2,6 +2,9 @@ import sys
 import pygame
 from pygame.locals import *
 
+
+'''
+
 white = 255, 255, 255
 blue = 0, 0, 255
 pygame.init()   # 初始化
@@ -18,6 +21,29 @@ while True:
     pygame.display.update()
 
 
+''' # 创建了一个  窗口 可持续 显示 并有用户反应
+
+'''
+
+pygame.init()
+screen = pygame.display.set_mode((600,500))
+pygame.display.set_caption('Drawing Circles')
+while True:
+    for event in pygame.event.get():
+        if event.type in (QUIT, KEYDOWN):
+            sys.exit()
+    screen.fill((0,0,200))
+
+    # draw a circle
+    color = 255,255,0
+    position = 300,250 # 位置
+    radius = 100  # 大小
+    width = 10  # 线条的 粗细
+    pygame.draw.circle(screen,color,position,radius,width)
+
+    pygame.display.update()
+
+'''  # 绘制园
 
 
 
